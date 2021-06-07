e5a88771f1a17784c354c493c767347a0db62a52
Felipe Bezerra <fbezerra.tsi@gmail.com>
2021-06-07 18:30:00 -0300

    add remote port

--- src/server.js
+++ src/server.js
@@ -9,6 +9,6 @@ require('./config/db')
 app.use(express.json())
 app.use(cors())
 app.use(routes)
-app.listen('3000', () => {
+app.listen(process.env.PORT || '3000', () => {
     console.log('Executando...')
 })
